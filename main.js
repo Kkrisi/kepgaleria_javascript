8304f9f7a6c2be231947bdfd26ecf58a7b321255
Kádár Kristóf Péter (SZF_2023_2025) <kadar.kristof.peter1@diak.szamalk-szalezi.hu>
2024-03-07 09:36:38 +0100

    javitasorai

--- main.js
+++ main.js
@@ -11,22 +11,62 @@ galeriaELem.innerHTML += htmlOsszeAllit(KEPEK)
 
 
 /*2. feladat  A .nagykep divben lévő img elem megfogása */
+const nagykepImgELem = document.querySelector(".nagykep img")
 
 /* A .nagykep div megfogása */
-
+const nagykepELem = document.querySelector(".nagykep")
 
 
 /*3. feladat-  eseménykezelő hozzáadása a kisképekhez  */
 /* Az előzőekben létrehozott kisképek megfogása (querySelectorAll) - ez egy lista lesz */
-
-/* eseménykezelő hozzáadása a kisképekhez, ciklussal */
+const kiskepElemek = document.querySelectorAll(".kep") //lista
+for (let i = 0; i < kiskepElemek.length; i++) {
+    /* eseménykezelő hozzáadása a kisképekhez, ciklussal */
+    kiskepElemek[i].addEventListener("click", function (event){
+        console.log(event.target.src)
+        nagykepImgELem.src = event.target.src
+    }
+    )
+}
 
 
 
 
 /* Az eseménykezelő függvényében: ha a kisképre kattintunk, akkor a nagykép src attribútumát le kell cserélni a kiskép src attribútumában megadott elérési útvonalra  */
 
-
-
+let aktIndex = 0;
 
 /* 4. feladat .bal és a .jobb gombokra kattintva léptessük a galériát */
+/* 1. Fogd meg a jobb gombot*/
+const jobbElem = document.getElementsByClassName("jobb")[0]
+const balElem = document.getElementsByClassName("bal")[0]
+
+/*2. Tegyél rá egy eseménykezelőt*/
+jobbElem.addEventListener("click", jobbraKatt)
+balElem.addEventListener("click", balraKatt)
+
+/*3. Az eseménykezelőben az aktindex változó értékét növeld*/
+/* aktindex változónak megfelelp kép elérési utját cseréld le a nagykepelem img src-jében*/
+function jobbraKatt(){
+    aktIndex++;
+    //aktIndex = aktIndex % KEPEK.length;
+    if(aktIndex >= KEPEK.length){
+        aktIndex = 0;
+    }
+    console.log(aktIndex)
+    console.log(KEPEK[aktIndex].kep)
+    nagykepImgELem.src = KEPEK[aktIndex].kep
+}
+
+
+function balraKatt(){
+    aktIndex++;
+    if(aktIndex >= 0){
+        aktIndex = KEPEK.length;
+    }
+    console.log(aktIndex)
+    console.log(KEPEK[aktIndex].kep)
+    nagykepImgELem.src = KEPEK[aktIndex].kep
+}
+
+
